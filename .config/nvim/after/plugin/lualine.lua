e352d449b9ed52ab64c929f558426a9b8a5fb144
Kenneth Ho <8515878+kenesu-h@users.noreply.github.com>
2023-08-05 13:21:05 -0400

    lualine bubble theming

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -3,4 +3,16 @@ require("lualine").setup({
     section_separators = { left = "", right = "" },
     component_separators = { left = "", right = "" },
   },
+  sections = {
+    lualine_a = {
+      { 'mode', separator = { left = '' }, right_padding = 2 },
+    },
+    lualine_b = { 'filename', 'branch' },
+    lualine_c = { 'fileformat' },
+    lualine_x = {},
+    lualine_y = { 'filetype', 'progress' },
+    lualine_z = {
+      { 'location', separator = { right = '' }, left_padding = 2 },
+    },
+  },
 })
