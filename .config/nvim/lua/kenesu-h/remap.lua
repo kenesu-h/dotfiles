31aefdd48942e04087c7c60b1e30a35d0c72edb4
Kenneth Ho <8515878+kenesu-h@users.noreply.github.com>
2023-10-19 22:37:41 +0000

    Adjusted block movement remaps

--- .config/nvim/lua/kenesu-h/remap.lua
+++ .config/nvim/lua/kenesu-h/remap.lua
@@ -1,8 +1,8 @@
 vim.g.mapleader = " "
 
 -- Thanks to ThePrimeagen (https://www.youtube.com/watch?v=w7i4amO_zaE)
-vim.keymap.set("v", "N", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "B", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
