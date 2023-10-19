fd0558bac322d821d2018818a2354be7c994c496
Kenneth Ho <8515878+kenesu-h@users.noreply.github.com>
2023-10-19 22:38:11 +0000

    Hopefully fixed luasnip jumping behavior

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -24,13 +24,14 @@ local cmp = require("cmp")
 local luasnip = require("luasnip")
 
 -- https://www.reddit.com/r/neovim/comments/wmkf9o/how_to_use_tab_and_shifttab_to_cycle_through/
+-- https://www.reddit.com/r/neovim/comments/z9os8x/strange_behaviour_cursor_jumping_with_tabkey/
 cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
+      elseif luasnip.expandable() then
+        luasnip.expand()
       else
         fallback()
       end
@@ -38,8 +39,6 @@ cmp.setup({
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
       else
         fallback()
       end
